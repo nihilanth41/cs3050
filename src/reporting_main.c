789e1fce2312032002cb27f56923ff6071eb6beb
Zachary Rump <nihilanth41@gmail.com>
2015-08-28 19:04:35 -0500

    static allocated arrays

--- src/reporting_main.c
+++ src/reporting_main.c
@@ -6,6 +6,9 @@
 //temporarily?
 #define ARRAY_SIZE 100
 
+long int mode(int *array, int size);
+void sort(int *array, int size);
+
 int main( int argc, char **argv )
 {
 	//for checking return values
@@ -21,23 +24,50 @@ int main( int argc, char **argv )
 	FILE *file = fopen( argv[1], "r" );
 	if(file == NULL) { return FILE_FAILED_TO_OPEN; }
 	//file can have any size from 0 to MAX_INT
-	int *a = malloc(sizeof(int));
-	long int count = 0;
+	int count=0;	
 	for(;;)
 	{
-		ret = fscanf(file, "%d", a);
+		ret = fscanf(file, "%s", buf);
 		if(ret == EOF) break;
-		printf("Number [%ld] is %d\n", count, *a);
+		//printf("Number [%ld] is %s\n", count, buf);
 		count++;
 	}
-	free(a);
 	if(count == 0) return PARSING_ERROR_EMPTY_FILE;
+	char **numbers = (char **)malloc(sizeof(char *)*count*100);
+
+
+	
+		
+	
+	//close file
 	ret = fclose(file);
 	if(ret != 0) return FILE_FAILED_TO_CLOSE; 
+	
 	// These are here to showcase use of the reporting function and can be removed
 	long int mode = 14;
+	
 	// Call this function with the proper arguments, in the correct order (count,mode)
 	REPORT(count, mode);
 	return EXIT_SUCCESS;
 
+}
+
+
+void sort(int *p, int size)
+{ //merge sort 
+	int hold = 0;
+	int pass = 0;
+	for(pass=1; pass<size; pass++)
+	{
+		int i=0;
+		for(i=0; i<(size-1); i++)
+		{
+			if(*(p+i) > *(p+i+1)) //if larger value appears in smaller index then swap
+			{
+				hold = *(p+i); //hold larger value
+				*(p+i) = *(p+i+1); //swap smaller value into smaller index
+				*(p+i+1) = hold; //swap larger value with larger index		
+			}
+		}
+	}
 }
