6b86f5a98b190f98fcf450d718188fad35bf7f0d
Zachary Rump <nihilanth41@gmail.com>
2015-08-29 15:45:14 -0500

    malloc strings using strdup and store them in an array

--- src/reporting_main.c
+++ src/reporting_main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "statistics_errors.h"
 #include "statistics_reporting.h"
 
-//temporarily?
-#define ARRAY_SIZE 100
+//max str length
+#define SIZE_MAX 100 
 
 long int mode(int *array, int size);
 void sort(int *array, int size);
 
 int main( int argc, char **argv )
 {
-	//for checking return values
-	static int ret=0;
-
 	//check input args
 	if(argc != 2) 
 	{
@@ -24,28 +22,53 @@ int main( int argc, char **argv )
 	FILE *file = fopen( argv[1], "r" );
 	if(file == NULL) { return FILE_FAILED_TO_OPEN; }
 	//file can have any size from 0 to MAX_INT
-	int count=0;	
+	//get the number of strings
+	static int ret;
+	long int count=0;
+	char buf[SIZE_MAX];
 	for(;;)
 	{
-		ret = fscanf(file, "%s", buf);
+		ret = fscanf(file, "%99s", buf);
 		if(ret == EOF) break;
-		//printf("Number [%ld] is %s\n", count, buf);
 		count++;
 	}
 	if(count == 0) return PARSING_ERROR_EMPTY_FILE;
-	char **numbers = (char **)malloc(sizeof(char *)*count*100);
-
-
-	
-		
-	
 	//close file
 	ret = fclose(file);
 	if(ret != 0) return FILE_FAILED_TO_CLOSE; 
 	
+	//alloc memory for string pointers 
+	char **pp = (char **)malloc(count * sizeof(char *));
+	char **begin = pp;
+	//reopen file
+	file = fopen( argv[1], "r" );
+	if(file == NULL) { return FILE_FAILED_TO_OPEN; }
+	//only read as many strings as we allocated memory for
+	int i=0;
+	for(i=0; i<count; ++i)
+	{
+		//get string from file
+		ret = fscanf(file, "%99s", buf);
+		//allocate memory for string
+		//alternatively use sizeof(buf)
+		*pp = strdup(buf);
+		pp++;	
+		//point string pointer at string 
+		//increment string pointer
+		if(ret == EOF) break;
+	}
+	pp = begin;
+	for(i=0; i<count; i++)
+	{
+		printf("%s\n", *pp);
+		pp++;
+	}
+
+	
+		
 	// These are here to showcase use of the reporting function and can be removed
 	long int mode = 14;
-	
+			
 	// Call this function with the proper arguments, in the correct order (count,mode)
 	REPORT(count, mode);
 	return EXIT_SUCCESS;
